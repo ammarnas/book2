0c1e36d254365c32a077854fdb035f5ae078c34e
ammarnas <ammarnasser.contact@gmail.com>
2022-09-26 21:36:36 +0300

    userControl

---
+++ Book2/Book2/Data/BookDomain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Book2.Data
+{
+    class BookDomain
+    {
+        public static void AddDefualtUser()
+        {
+            using (Book_ManagementEntities Context = new Book_ManagementEntities())
+            {
+                int IsAddUser = Context.Users.Count();
+                if (IsAddUser == 0)
+                {
+                    string Contents = File.ReadAllText(@"C:\Users\Ammar\source\repos\book2\Book2\Book2\Data\DefualtUser.json");
+                    var DefualtUser = JsonSerializer.Deserialize<List<User>>(Contents);
+                    Context.Users.AddRange(DefualtUser);
+                    Context.SaveChanges();
+                }
+            }
+        }
+
+        // Login
+        public static void Login()
+        {
+            try
+            {
+            login:
+                Console.Write("Enter Username : ");
+                string Username = Console.ReadLine();
+
+                Console.Write("Enter Password : ");
+                string Password = Console.ReadLine();
+
+                using (Book_ManagementEntities Context = new Book_ManagementEntities())
+                {
+                    var user = Context.Users.Where(u => u.Username == Username && u.Password == Password).ToList();
+                    if (user.Count == 0)
+                    {
+                        Console.WriteLine("Username or Password uncorrect");
+                        Console.WriteLine("Try Again");
+                        goto login;
+                    }
+                    Console.WriteLine("Login Successfully");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        // Count of book
+        public static void BookCount()
+        {
+            using (Book_ManagementEntities Context = new Book_ManagementEntities())
+            {
+                var Count = Context.Books.Select(u => u.Title).Distinct().Count();
+                Console.WriteLine($"you have {Count} Books in store\n");
+            }
+        }
+        // User Control
+        public static void UserControl()
+        {
+
+            Console.WriteLine("- Enter 1 To Show Info Of Books");
+            Console.WriteLine("- Enter 2 To Show All Info Of Books");
+            Console.WriteLine("- Enter 3 To Delete Book");
+            Console.WriteLine("- Enter 4 To Edit Book");
+        }
+        // 
+    }
+}
--- Book2/Book2/Program.cs
+++ Book2/Book2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Book2.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,31 @@ namespace Book2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Start Project....\n");
+            BookDomain.AddDefualtUser();
+
+            BookDomain.Login();
+
+            BookDomain.UserControl();
+
+            int WordControl = Convert.ToInt32(Console.ReadLine());
+
+            switch (WordControl)
+            {
+                case 1:
+                    Console.WriteLine("");
+                    break;
+                case 2:
+                    Console.WriteLine("");
+                    break;
+                case 3:
+                    Console.WriteLine("");
+                    break;
+                case 4:
+                    Console.WriteLine("");
+                    break;
+                
+            }
         }
     }
 }
